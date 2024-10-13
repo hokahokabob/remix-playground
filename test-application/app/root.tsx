72f315d18b5c794e0dd7c6316793d2f803e5c3c9
yuuta <>
2024-10-13 18:09:39 +0900

    let basic layout with sidebar be applied for every screen

--- test-application/app/root.tsx
+++ test-application/app/root.tsx
@@ -1,20 +1,89 @@
 import {
+  ClientActionFunctionArgs,
+  ClientLoaderFunctionArgs,
+  Form,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "@remix-run/react";
+  ScrollRestoration,
+  useLoaderData,
+} from '@remix-run/react'
 
-import { LinksFunction } from "@remix-run/node";
-import appStylesHref from "./app.css?url";
+import { LinksFunction } from '@remix-run/node'
+import appStylesHref from './app.css?url'
+import { createEmptyContact, getContacts } from './data'
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: appStylesHref },
-];
+  { rel: 'stylesheet', href: appStylesHref },
+]
 
-export const App = () => {  
-  return <Outlet />;
+export const clientLoader = async (request: ClientLoaderFunctionArgs) => {
+  const contacts = await getContacts()
+
+  return { contacts }
+}
+
+export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
+  const contact = await createEmptyContact()
+  return { contact }
+}
+
+export const App = () => {
+  const { contacts } = useLoaderData<typeof clientLoader>()
+  return (
+    <>
+      <div id="sidebar">
+        <h1>Remix Contacts</h1>
+        <div>
+          <Form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search conPtacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{' '}
+                    {contact.favorite ? <span>★</span> : null}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+
+      <ScrollRestoration />
+      <Scripts />
+    </>
+  )
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -32,11 +101,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export function HydrateFallback() {
   return <p>Loading...</p>
 }
 
-export default App;
+export default App
--- test-application/app/routes/_index.tsx
+++ test-application/app/routes/_index.tsx
@@ -1,86 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
-import { ClientActionFunctionArgs, ClientLoaderFunctionArgs, Form, Link, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
-import { createEmptyContact, getContacts } from "~/data";
+import type { MetaFunction } from '@remix-run/node'
+import { Outlet } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
-export const clientLoader = async (request: ClientLoaderFunctionArgs) => {
-  const contacts = await getContacts();
-  
-  return { contacts };
-}
-
-
-export const clientAction = async ({
-  request,
-}: ClientActionFunctionArgs) => {
-  const contact  = await createEmptyContact();
-  return { contact };
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ]
 }
 
 export const Index = () => {
-  // const { contacts } = useLoaderData() as { contacts: ContactRecord[] };
-  const { contacts } = useLoaderData<typeof clientLoader>();
-
-  return (
-    <>
-    <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite ? (
-                        <span>★</span>
-                      ) : null}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
-        </div>
-        <div id="detail">
-          <Outlet />
-        </div>
-
-        <ScrollRestoration />
-        <Scripts />
-    </>
-  )  
+  return <Outlet />
 }
 
-export default Index;
+export default Index
